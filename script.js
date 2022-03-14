8488718dc746c6c188492f25840617ecec8b0e44
anjalidalal <anjalimaria2002@gmail.com>
2022-03-14 23:56:47 +0530

    Responsive webpage(laptop view)

--- script.js
+++ script.js
@@ -24,36 +24,40 @@ fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
         });
 
     })
+    let randomBtn = document.getElementById("random");
 
-const author = "Bill Gates";
-let bttn = document.getElementById("bttn");
-let main = document.getElementById("main");
-
-fetch(`https://quote-garden.herokuapp.com/api/v3/quotes?author=${author}&limit=3`)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log(data);
-        data.data.forEach(function (el) {
-            let subDiv = document.createElement("div");
-            subDiv.setAttribute("class", "subDiv");
-
+    randomBtn.addEventListener("click", function() {
+        fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            mainDiv.textContent = "";
             
-            const yellowDiv = document.createElement("div");
-            yellowDiv.setAttribute("class", "line");
-
-            const p = document.createElement("p");
-            p.textContent = el.quoteText;
-            p.setAttribute("class", "p")
-
-            subDiv.append(yellowDiv, p);
-            main.appendChild(subDiv);
-        });
-
+            data.data.forEach(function (el) {
+                let subDiv = document.createElement("div");
+                subDiv.setAttribute("class", "subDiv");
+    
+                
+                const yellowDiv = document.createElement("div");
+                yellowDiv.setAttribute("class", "line");
+    
+                const p = document.createElement("p");
+                p.textContent = el.quoteText;
+    
+                subDiv.append(yellowDiv, p);
+                mainDiv.appendChild(subDiv);
+    
+                const nameA = document.querySelector(".aName");
+                nameA.textContent = el.quoteAuthor;
+    
+                const genreA = document.querySelector(".aGenre");
+                genreA.textContent = el.quoteGenre;
+                
+                localStorage.setItem("author", JSON.stringify(el.quoteAuthor))
+    
+            });
+    
+        })
     })
-
-
-function showDiv() {
-
-}
